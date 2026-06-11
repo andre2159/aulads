8a3d18aab301d226805d329c84bf1ca0662f8a85
andre.ramos <andre.ramos@aluno.edu.pi.gov.br>
2026-06-11 17:46:58 +0000

    Andrecalculaadora

---
+++ calculadora.js
@@ -0,0 +1,33 @@
+// calculadora.js
+
+function soma(a, b) {
+    return a + b;
+  }
+  
+  function subtracao(a, b) {
+    return a - b;
+  }
+  
+  function multiplicacao(a, b) {
+    return a * b;
+  }
+  
+  function divisao(a, b) {
+    if (b === 0) {
+      return "Erro: divisão por zero";
+    }
+    return a / b;
+  }
+  
+
+  
+  
+  module.exports= {
+    soma:soma,
+    divisao:divisao,
+    multiplicacao:multiplicacao,
+    subtracao:subtracao 
+
+}
+
+  
