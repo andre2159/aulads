41b4f40b3cffd11523f5f5c8fde463af2f13d239
andre.ramos <andre.ramos@aluno.edu.pi.gov.br>
2026-07-01 17:06:29 +0000

    andre remover

--- blibioteca.js
+++ blibioteca.js
@@ -41,8 +41,40 @@ console.log("livro adicionado:",livro.nome)
                 }
             }
         },
+        devolver (nome){
+            for(let i=0;i<this.estante.length;i++){
+                const livro=this.estante[i];
+                if(livro.nome===nome){
+                    if(livro.disponivel===false){
+                        livro.disponivel=true;
+                        livro.alugado=false;
+                        livro.estoque+=1.
+                        console.log("livro",+nome,+"delvovido");
+                        return;}
+                        else{console.log("o",+nome,+"ja está disponivel");
+                            return;
+                        }
+                    }
+                    }
+                    console.log("o",+nome,+"não foi encontrado");
+                },
+removerlivro(nome){
+    for(let i=0;i<this.estante.length;i++){
+        if (this.estante[i].nome.toLowerCase() === nome.toLowerCase()) {
 
-}// biblioteca
+        
+            const removido=this.estante[i]
+            this.estante.splice(i,1);
+            console.log("livro removido",+removido.nome);
+            return;
+        }}
+        console.log("livro não encontrado");
+    }
+};
+
+
+
+// biblioteca
 biblioteca.adicionarlivro(
 "a era de um deus",
 10
@@ -54,3 +86,7 @@ biblioteca.adicionarlivro(
 
 biblioteca.mostrarLivros()
 biblioteca.aluguel("a era de um deus");
+biblioteca.devolver("a era de um deus");
+biblioteca.removerlivro("a era de um deus");
+
+
